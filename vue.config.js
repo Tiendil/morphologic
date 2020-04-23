da829ca0e1afafb7c00a228333ce419ef362cd63
Tiendil <a.eletsky@gmail.com>
2020-04-23 18:01:25 +0300

    site files

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     "lintOnSave": false,
+
     "runtimeCompiler": true, // TODO: on release: https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only ?
+
     "transpileDependencies": [
         "vuetify"
-    ]
+    ],
+
+    "publicPath": process.env.NODE_ENV === 'production' ? '/morphology/' : '/'
 }
