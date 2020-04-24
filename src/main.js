4992d994b39c626269b9752041a81b958f0362af
Tiendil <a.eletsky@gmail.com>
2020-04-24 06:51:18 +0300

    simple google analytics integration

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueGtag from "vue-gtag";
+
+
+Vue.use(VueGtag, {
+  config: { id: "UA-10915391-7" }
+});
+
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
